a3628259220626b4387acf2a5a727ce65d56d7be
Leo <neotheone458@gmail.com>
2022-06-19 18:27:17 +0530

    added testimonial slider

--- js/script.js
+++ js/script.js
@@ -1,9 +1,39 @@
-let navbar = document.querySelector('.header .navbar');
+let navbar = document.querySelector(".header .navbar")
 
-document.querySelector('#menu-btn').onclick = () =>{
-    navbar.classList.toggle('active');
+document.querySelector("#menu-btn").onclick = () => {
+	navbar.classList.toggle("active")
 }
 
-window.onscroll = () =>{
-    navbar.classList.remove('active');
+window.onscroll = () => {
+	navbar.classList.remove("active")
 }
+
+var swiper = new Swiper(".slide-content", {
+	slidesPerView: 3,
+	spaceBetween: 25,
+	loop: true,
+	centerSlide: "true",
+	fade: "true",
+	grabCursor: "true",
+	pagination: {
+		el: ".swiper-pagination",
+		clickable: true,
+		dynamicBullets: true,
+	},
+	navigation: {
+		nextEl: ".swiper-button-next",
+		prevEl: ".swiper-button-prev",
+	},
+
+	breakpoints: {
+		0: {
+			slidesPerView: 1,
+		},
+		520: {
+			slidesPerView: 2,
+		},
+		950: {
+			slidesPerView: 3,
+		},
+	},
+})
