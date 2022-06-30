7657a90f7976829eca7f882ba26ea0a48639fb10
super7000 <subratachowdhury7000@gmail.com>
2022-06-30 19:50:52 +0530

    couses + to courses -

--- home.php
+++ home.php
@@ -22,6 +22,14 @@
 				padding: 1.5rem;
 			}
 		</style>
+		<script>
+			function plus_to_min() {
+				document.querySelector("#cos").innerHTML="courses -";
+			}
+			function min_to_plus() {
+				document.querySelector("#cos").innerHTML="courses +";
+			}
+		</script>
 	</head>
 	<body>
 		<!-- header section starts  -->
@@ -39,7 +47,7 @@
 					<li><a href="#testimonials">testimonials</a></li>
 					<li><a href="#about">about</a></li>
 					<li>
-						<a href="#">courses +</a>
+						<a href="#" id="cos" onmousemove="plus_to_min();" onmouseout="min_to_plus();">courses +</a>
 						<ul>
 							<li><a href="subject.php?class=11">class 11</a></li>
 							<li><a href="subject.php?class=12">class 12</a></li>
